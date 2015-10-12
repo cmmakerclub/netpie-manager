4becd26dd63a5dd03d4a043232269ed52af7d81d
Nat Weerawan <nat@opendream.co.th>
2015-10-12 19:46:07 +0700

    Rebuild

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@ rm -rvf dist
 gulp build
 git add . 
 git commit -m "Rebuild"
-git push  -u origin master 
+git push -u origin master 
+echo "NEXT UP"
 pushd ..
 git commit -am "rebuild"
 git push  -u origin master 
