cba7fa3b1b4ac87c5b48da56cee6d38a25d17988
Nat Weerawan <nat@opendream.co.th>
2015-10-18 15:40:24 +0700

    Add devices controller & views.

---
+++ src/app/devices/controllers/devices-controller.js
@@ -0,0 +1,16 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name netpieManager.controller:devicesCtrl
+ * @description
+ * # devicesCtrl
+ * Controller of the netpieManager
+ */
+angular.module('netpieManager')
+  .controller('devicesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
