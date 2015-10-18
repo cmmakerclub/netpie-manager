efc1a38ea1403d368c2164023d2dfda248140c78
Nat Weerawan <nat@opendream.co.th>
2015-10-18 13:39:03 +0700

    Use auto format code.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,92 +1,92 @@
 (function () {
   'use strict';
-    String.prototype.isEmpty = function() {
-        return (this.length === 0 || !this.trim());
-    };
+  String.prototype.isEmpty = function () {
+    return (this.length === 0 || !this.trim());
+  };
 
-    var guid = function (prefix) {
-      function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-          .toString(16)
-          .substring(1);
-      }
-      return s4() + "-" +new Date().getTime()
+  var guid = function (prefix) {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
     }
+    return s4() + "-" + new Date().getTime()
+  }
 
   angular
     .module('netpieManager')
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, toastr, $http, $log, 
-      $window, $mdSidenav, $localStorage, $scope) {
+  function MainController($timeout, toastr, $http, $log,
+    $window, $mdSidenav, $localStorage, $scope) {
 
-      var localStorageDefault = {
-        main: {
-          config: { 
-            prefix: "/ChiangMaiMakerClub",
-            host: "iot.eclipse.org",
-            myName: "",
-            port: 1883
-          },
-          checkbox: { clientId: true, userPass: false },
-          devices: []
-        }
-      };
-
-      $localStorage.$default(localStorageDefault);
-      $scope.config = $localStorage.main.config;
-
-      $scope.tab = $scope.config || {};
-      $scope.checkbox = $localStorage.main.checkbox || {};
-
-      $scope.$watch("checkbox.clientId", function  (current, old) {
-        if (current === true) {
-          $scope.config.clientId = guid("cmmc");
-        }
-        else {
-          $scope.config.clientId = "";
-        }
-        // $log.debug("check changed", arguments);
-      });
-
-      $scope.tabs = $localStorage.main.devices || [];
-      $scope.device_count = $scope.tabs.length;
-      $scope.selectedIndex = $scope.tabs.length;
-
-      this.generate = function() {
-        $log.debug('generate');
-        if ($scope.config.myName.isEmpty()) {
-          $log.error("EMPTY myName");
-        }
-        else {
-
-          if ( !$scope.checkbox.userPass ) {
-            $scope.config.password = "";
-            $scope.config.username = "";
-          }
-
-          var obj = angular.copy($scope.config);
-          $localStorage.main.devices.push(obj);
-          console.log($localStorage.main.devices);
-          $scope.config.clientId = guid("cmmc");
-          $scope.config.myName = "";
-          $scope.tabs = $localStorage.main.devices;
-        }
-      };
-
-      $scope.loadDevices = function () {
-        $scope.devices = $localStorage.main.devices;
-        return $localStorage.main.devices;
+    var localStorageDefault = {
+      main: {
+        config: {
+          prefix: "/ChiangMaiMakerClub",
+          host: "iot.eclipse.org",
+          myName: "",
+          port: 1883
+        },
+        checkbox: { clientId: true, userPass: false },
+        devices: []
       }
+    };
 
+    $localStorage.$default(localStorageDefault);
+    $scope.config = $localStorage.main.config;
 
-      this.clear = function() {
-        $log.debug('generate');
-        $localStorage.main.devices = [];
+    $scope.tab = $scope.config || {};
+    $scope.checkbox = $localStorage.main.checkbox || {};
+
+    $scope.$watch("checkbox.clientId", function (current, old) {
+      if (current === true) {
         $scope.config.clientId = guid("cmmc");
+      }
+      else {
+        $scope.config.clientId = "";
+      }
+      // $log.debug("check changed", arguments);
+    });
+
+    $scope.tabs = $localStorage.main.devices || [];
+    $scope.device_count = $scope.tabs.length;
+    $scope.selectedIndex = $scope.tabs.length;
+
+    this.generate = function () {
+      $log.debug('generate');
+      if ($scope.config.myName.isEmpty()) {
+        $log.error("EMPTY myName");
+      }
+      else {
+
+        if (!$scope.checkbox.userPass) {
+          $scope.config.password = "";
+          $scope.config.username = "";
+        }
+
+        var obj = angular.copy($scope.config);
+        $localStorage.main.devices.push(obj);
+        console.log($localStorage.main.devices);
+        $scope.config.clientId = guid("cmmc");
+        $scope.config.myName = "";
         $scope.tabs = $localStorage.main.devices;
-      };
+      }
+    };
+
+    $scope.loadDevices = function () {
+      $scope.devices = $localStorage.main.devices;
+      return $localStorage.main.devices;
+    }
+
+
+    this.clear = function () {
+      $log.debug('generate');
+      $localStorage.main.devices = [];
+      $scope.config.clientId = guid("cmmc");
+      $scope.tabs = $localStorage.main.devices;
+    };
   }// end controller
 
 
